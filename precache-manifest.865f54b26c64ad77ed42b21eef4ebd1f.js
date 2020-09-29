7a9e1f538528b1864975f68ee1e42e522581b4f9
Aakashcs <rockiing.aakash@gmail.com>
2020-09-29 15:30:17 +0000

    Deploying to gh-pages from  @ 0b4e3de5ae58f11b238c36c7d917af238174baab 🚀

---
+++ precache-manifest.865f54b26c64ad77ed42b21eef4ebd1f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6720d6f9b1f08cc1f3926ae1e8eb57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b87f2e3013a0e1ed347e",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "e1fc8f4926ba70a6a8a4",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "b486517e6ecb18836fa4",
+    "url": "/static/css/main.05d63473.chunk.css"
+  },
+  {
+    "revision": "04483d6cdaefa3404e74",
+    "url": "/static/js/2.b2a61fb8.chunk.js"
+  },
+  {
+    "revision": "b87f2e3013a0e1ed347e",
+    "url": "/static/js/3.a6804299.chunk.js"
+  },
+  {
+    "revision": "e1fc8f4926ba70a6a8a4",
+    "url": "/static/js/4.9cb94597.chunk.js"
+  },
+  {
+    "revision": "b486517e6ecb18836fa4",
+    "url": "/static/js/main.b9cbf884.chunk.js"
+  },
+  {
+    "revision": "d77d4248bd7f7d64dfa0",
+    "url": "/static/js/runtime-main.2417f9ed.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "20456736ffdb11982eabbadb768078df",
+    "url": "/static/media/ach1.20456736.jpg"
+  },
+  {
+    "revision": "cb3f640732da3a5e98fab85c90e8aac8",
+    "url": "/static/media/ach2.cb3f6407.jpg"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0cc093293715afce6e5343c62f779cb9",
+    "url": "/static/media/startup1.0cc09329.png"
+  },
+  {
+    "revision": "17dcf7e10994ce9007b711bfe7bd7c40",
+    "url": "/static/media/sukkur-iba.17dcf7e1.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
